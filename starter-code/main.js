e55e5b9ef4170ab8badcdbe68d92561fda49762e
Krisztian Groz <krisztiangroz@krisztians-mbp.home>
2017-01-17 00:05:09 +0000

    created game logic2

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,6 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "King";
 var cardFour = "King";
+
+
+ if (cardOne === cardTwo) {window.alert("You found a mach");} else window.alert("Ohh Boy");
