d54f7b9497b9551340ca683f5b34b490a008ac40
Krisztian Groz <krisztiangroz@krisztians-mbp.home>
2017-01-18 20:23:53 +0000

    created game logi8

--- starter-code/main.js
+++ starter-code/main.js
@@ -9,6 +9,55 @@ var cardFour = "King";
 
 var cards = ['queen', 'queen', 'king', 'king'];
 
+ var cardsInPlay =[] ;
+
+ 
+
+			 function createBoard() {
+
+			 	// loop in array cards 
+
+			   
+			   for (var i=0; i<cards.length; i++) {
+
+			
+			     // We make a Div element 
+			    var cardElement = document.createElement('div');
+
+
+			
+			    // get a class name for css
+			    cardElement.className = 'card';
+
+				
+			    
+			    // Set an attribute what we can use 
+			    cardElement.setAttribute('data-card', cards[i]);
+
+				// add a function for the click , what I have not created yet :)
+			    cardElement.addEventListener('click', isTwoCards);
+
+			 // we add the element to our board
+			    
+			    board.appendChild(cardElement);
+
+			   
+			  }
+			
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
  /*if (cardOne === cardTwo) {window.alert("You found a mach");} else window.alert("Ohh Boy try it again");*/
@@ -18,7 +67,7 @@ var cards = ['queen', 'queen', 'king', 'king'];
 /*
 var element = document.getElementById('game-board');
  
- for (var i = 0; i < 4; i++) {
+ for (var i = 0; i < 12; i++) {
  var divElement = document.createElement('Div');
  	divElement.className = ("card");
  element.appendChild(divElement);
